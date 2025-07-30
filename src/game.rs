759c6d643ff318089b25ab0abfd7ed6d4dfeaf68
Jakob <jakob@pop-os.home>
2025-07-30 20:17:27 +0200

    looks unpleasant, but we got some movement going

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{collections::VecDeque, time::Instant};
+
 use eframe::egui;
-use egui::Color32;
+use egui::{Color32, Direction, Key};
 
 fn main() -> eframe::Result<()> {
     env_logger::init();
@@ -17,39 +19,96 @@ fn main() -> eframe::Result<()> {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::<MyApp>::default()
+            Box::<MyRustSnakeGame
+            >::default()
         }),
     )
 }
 
-struct MyApp {
-    map_size: (u8, u8),
-    head_img: egui::ImageSource<'static>,
-    body_img: egui::ImageSource<'static>,
+struct GameState{
+    snake_length: u16,
+    player_direction: Direction,
+    player_position: (u16, u16),
+    body_parts: VecDeque<(u16,u16)>,
 }
 
-impl Default for MyApp {
+impl Default for GameState{
+    fn default() -> Self {
+        Self { 
+            snake_length: 0,
+            player_direction: Direction::TopDown,
+            player_position: (5, 5),
+            body_parts: VecDeque::new(),
+        }
+    }
+}
+
+struct MyRustSnakeGame
+ {
+    map_size: (u8, u8),
+    head_img: egui::ImageSource<'static>,
+    head_img_left: egui::ImageSource<'static>,
+    head_img_up: egui::ImageSource<'static>,
+    head_img_right: egui::ImageSource<'static>,
+    body_img: egui::ImageSource<'static>,
+    elapsed_time:f32,
+    last_update: std::time::Instant,
+    game_state: GameState,
+
+}
+
+impl Default for MyRustSnakeGame
+ {
     fn default() -> Self {
         Self { 
             map_size: (15, 17),
             head_img: egui::include_image!("assets/snake_head.png"),
+            head_img_left: egui::include_image!("assets/snake_head_left.png"),
+            head_img_up: egui::include_image!("assets/snake_head_up.png"),
+            head_img_right: egui::include_image!("assets/snake_head_right.png"),
             body_img: egui::include_image!("assets/snake_body.png"),
+            last_update: Instant::now(),
+            elapsed_time: 0.0,
+            game_state: GameState::default()
          }
     }
 }
 
-impl eframe::App for MyApp {
+impl eframe::App for MyRustSnakeGame
+ {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default()
             .frame(egui::Frame::none().fill(Color32::BLACK))
             .show(ctx, |ui| {
-                self.draw_background(ui, self.map_size);
-                self.draw_player(ui, (0, 0), 0.0, self.map_size);
+                self.handle_input(ui);
+                self.game_logic();
+                self.draw_game(ui);
             });
+        ctx.request_repaint();
     }
 }
 
-impl MyApp {
+impl MyRustSnakeGame
+ {
+
+    fn game_logic(&mut self){
+        let now = std::time::Instant::now();
+        let dt = (now - self.last_update).as_secs_f32();
+        self.last_update = now;
+        self.elapsed_time += dt;
+
+        let update_interval = 0.5; // seconds
+        while self.elapsed_time >= update_interval {
+            self.game_update();
+            self.elapsed_time -= update_interval;
+        }
+    }
+
+    fn draw_game(&mut self, ui: &egui::Ui){
+                self.draw_background(ui, self.map_size);
+                self.draw_player(ui);
+    }
+
     fn draw_background(&self, ui: &egui::Ui, map_size: (u8, u8)) {
         let painter = ui.painter();
         let mut rect = ui.max_rect(); // The whole panel
@@ -65,7 +124,7 @@ impl MyApp {
             rect.set_width(rect.height());
         }
 
-        painter.rect_filled(rect, 0.0, Color32::WHITE); // Fill background
+        painter.rect_filled(rect, 0.0, Color32::GRAY); // Fill background
         painter.rect_stroke(rect, 0.0, (2.0, Color32::BLUE)); // Red border
 
         let rows = map_size.0 as usize;
@@ -90,39 +149,89 @@ impl MyApp {
         }
     }
 
+    fn handle_input(&mut self, ui: &egui::Ui){
+        ui.input(|inp| {
+            if inp.keys_down.len() > 0 {
+                if inp.key_down(Key::ArrowDown) || inp.key_down(Key::S) {
+                    self.game_state.player_direction = Direction::TopDown;
+                } else if inp.key_down(Key::ArrowUp) || inp.key_down(Key::W) {
+                    self.game_state.player_direction = Direction::BottomUp;
+                } else if inp.key_down(Key::ArrowRight) || inp.key_down(Key::D) {
+                    self.game_state.player_direction = Direction::LeftToRight;
+                } else if inp.key_down(Key::ArrowLeft) || inp.key_down(Key::A) {
+                    self.game_state.player_direction = Direction::RightToLeft;
+                }
+            }
+        });
+    }
+
     fn draw_player(
+        &mut self,
+        ui: &egui::Ui,
+    ) {
+        for part in self.game_state.body_parts.clone() {
+            self.draw_at_cell(ui, self.body_img.clone(), part, self.map_size);
+        }
+
+        let head_img = match self.game_state.player_direction {
+            Direction::TopDown => &self.head_img,
+            Direction::BottomUp => &self.head_img_up,
+            Direction::LeftToRight => &self.head_img_right,
+            Direction::RightToLeft => &self.head_img_left,
+        };
+        self.draw_at_cell(ui, head_img.clone(), self.game_state.player_position, self.map_size);
+    }
+
+    fn game_update(&mut self){
+
+        self.game_state.body_parts.push_front(self.game_state.player_position);
+        if self.game_state.body_parts.len() > self.game_state.snake_length as usize {
+            self.game_state.body_parts.pop_back();
+        }
+
+        match self.game_state.player_direction {
+            Direction::TopDown => {
+                self.game_state.player_position.0 = (self.game_state.player_position.0 + 1) % self.map_size.0 as u16;
+            },
+            Direction::BottomUp => {
+                self.game_state.player_position.0 = (self.game_state.player_position.0 + self.map_size.0 as u16 - 1) % self.map_size.0 as u16;
+            },
+            Direction::LeftToRight => {
+                self.game_state.player_position.1 = (self.game_state.player_position.1 + 1) % self.map_size.1 as u16;
+            },
+            Direction::RightToLeft => {
+                self.game_state.player_position.1 = (self.game_state.player_position.1 + self.map_size.1 as u16 - 1) % self.map_size.1 as u16;
+            },
+        }
+        // Here you would also check for collisions with the walls, food, etc.
+
+
+    }
+
+    fn draw_at_cell(
         &self,
         ui: &egui::Ui,
-        grid_pos: (usize, usize),
-        _direction: f32,
+        image: egui::ImageSource<'static>,
+        grid_pos: (u16, u16),
         map_size: (u8, u8),
     ) {
         let rows = map_size.0 as usize;
         let cols = map_size.1 as usize;
         let mut rect = ui.max_rect();
-        // Make it a square and center that shit
+
+        // Make it a square and center it
         if rect.width() < rect.height() {
             rect.min.y += (rect.height() - rect.width()) / 2.0;
-            rect.max.y += (rect.height() - rect.width()) / 2.0;
             rect.set_height(rect.width());
         } else {
             rect.min.x += (rect.width() - rect.height()) / 2.0;
-            rect.max.x += (rect.width() - rect.height()) / 2.0;
             rect.set_width(rect.height());
         }
-        
+
         let cell_width = rect.width() / cols as f32;
         let cell_height = rect.height() / rows as f32;
 
         let tile_rect = egui::Rect::from_min_size(
-            egui::pos2(
-                rect.left() + grid_pos.1 as f32 * cell_width,
-                rect.top() + (grid_pos.0 + 1) as f32 * cell_height,
-            ),
-            egui::vec2(cell_width, cell_height),
-        );
-
-         let tile_rect_body = egui::Rect::from_min_size(
             egui::pos2(
                 rect.left() + grid_pos.1 as f32 * cell_width,
                 rect.top() + grid_pos.0 as f32 * cell_height,
@@ -130,9 +239,7 @@ impl MyApp {
             egui::vec2(cell_width, cell_height),
         );
 
-        // should clone just the reference not the image making it cheap ig
-        egui::Image::new(self.head_img.clone()).paint_at(ui, tile_rect);
-
-        egui::Image::new(self.body_img.clone()).paint_at(ui, tile_rect_body);
+        egui::Image::new(image).paint_at(ui, tile_rect);
     }
+
 }
