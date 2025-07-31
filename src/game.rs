66363a180caf3b94786b7ab77795b44cacf1a8e4
Jakob <jakob@pop-os.home>
2025-07-31 20:19:17 +0200

    made that body move

--- src/game.rs
+++ src/game.rs
@@ -4,6 +4,7 @@ use std::{collections::VecDeque, time::Instant};
 
 use eframe::egui;
 use egui::{Color32, Direction, Key};
+use rand::prelude::*;
 
 fn main() -> eframe::Result<()> {
     env_logger::init();
@@ -19,63 +20,78 @@ fn main() -> eframe::Result<()> {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::<MyRustSnakeGame
-            >::default()
+            Box::<MyRustSnakeGame>::default()
         }),
     )
 }
 
-struct GameState{
+struct GameState {
     snake_length: u16,
     player_direction: Direction,
+    player_draw_direction: Direction,
     player_position: (u16, u16),
-    body_parts: VecDeque<(u16,u16)>,
+    body_parts: VecDeque<(u16, u16)>,
 }
 
-impl Default for GameState{
+impl Default for GameState {
     fn default() -> Self {
-        Self { 
+        Self {
             snake_length: 0,
             player_direction: Direction::TopDown,
+            player_draw_direction: Direction::TopDown,
             player_position: (5, 5),
             body_parts: VecDeque::new(),
         }
     }
 }
 
-struct MyRustSnakeGame
- {
+struct Timer {
+    elapsed_time: f32,
+    last_update: Instant,
+}
+
+impl Default for Timer {
+    fn default() -> Self {
+        Self {
+            elapsed_time: 0.0,
+            last_update: Instant::now(),
+        }
+    }
+}
+
+struct MyRustSnakeGame {
     map_size: (u8, u8),
     head_img: egui::ImageSource<'static>,
     head_img_left: egui::ImageSource<'static>,
     head_img_up: egui::ImageSource<'static>,
     head_img_right: egui::ImageSource<'static>,
     body_img: egui::ImageSource<'static>,
-    elapsed_time:f32,
-    last_update: std::time::Instant,
+    food_img: egui::ImageSource<'static>,
+    move_timer: Timer,
+    food_timer: Timer,
     game_state: GameState,
-
+    food_location: Option<(u16, u16)>,
 }
 
-impl Default for MyRustSnakeGame
- {
+impl Default for MyRustSnakeGame {
     fn default() -> Self {
-        Self { 
+        Self {
             map_size: (15, 17),
             head_img: egui::include_image!("assets/snake_head.png"),
             head_img_left: egui::include_image!("assets/snake_head_left.png"),
             head_img_up: egui::include_image!("assets/snake_head_up.png"),
             head_img_right: egui::include_image!("assets/snake_head_right.png"),
             body_img: egui::include_image!("assets/snake_body.png"),
-            last_update: Instant::now(),
-            elapsed_time: 0.0,
-            game_state: GameState::default()
-         }
+            food_img: egui::include_image!("assets/apple.png"),
+            move_timer: Timer::default(),
+            food_timer: Timer::default(),
+            game_state: GameState::default(),
+            food_location: None,
+        }
     }
 }
 
-impl eframe::App for MyRustSnakeGame
- {
+impl eframe::App for MyRustSnakeGame {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default()
             .frame(egui::Frame::none().fill(Color32::BLACK))
@@ -88,25 +104,47 @@ impl eframe::App for MyRustSnakeGame
     }
 }
 
-impl MyRustSnakeGame
- {
-
-    fn game_logic(&mut self){
+impl MyRustSnakeGame {
+    fn game_logic(&mut self) {
         let now = std::time::Instant::now();
-        let dt = (now - self.last_update).as_secs_f32();
-        self.last_update = now;
-        self.elapsed_time += dt;
 
-        let update_interval = 0.5; // seconds
-        while self.elapsed_time >= update_interval {
+        // move timer
+        let mut dt = (now - self.move_timer.last_update).as_secs_f32();
+        self.move_timer.last_update = now;
+        self.move_timer.elapsed_time += dt;
+
+        let mut update_interval = 0.5; // seconds
+        if self.move_timer.elapsed_time >= update_interval {
             self.game_update();
-            self.elapsed_time -= update_interval;
+            self.move_timer.elapsed_time -= update_interval;
+        }
+
+        // food timer
+        dt = (now - self.food_timer.last_update).as_secs_f32();
+        self.food_timer.last_update = now;
+        self.food_timer.elapsed_time += dt;
+
+        update_interval = 5.0; // seconds
+        if self.food_timer.elapsed_time >= update_interval {
+            self.spawn_food();
+            self.food_timer.elapsed_time -= update_interval;
         }
     }
 
-    fn draw_game(&mut self, ui: &egui::Ui){
-                self.draw_background(ui, self.map_size);
-                self.draw_player(ui);
+    fn spawn_food(&mut self) {
+        if self.food_location.is_some() {
+            return;
+        }
+        let mut rng = rand::rng();
+        self.food_location = Some((
+            rng.random::<u16>() % (self.map_size.0 as u16),
+            rng.random::<u16>() % (self.map_size.1 as u16),
+        ));
+    }
+
+    fn draw_game(&mut self, ui: &egui::Ui) {
+        self.draw_background(ui, self.map_size);
+        self.draw_player(ui);
     }
 
     fn draw_background(&self, ui: &egui::Ui, map_size: (u8, u8)) {
@@ -149,7 +187,7 @@ impl MyRustSnakeGame
         }
     }
 
-    fn handle_input(&mut self, ui: &egui::Ui){
+    fn handle_input(&mut self, ui: &egui::Ui) {
         ui.input(|inp| {
             if inp.keys_down.len() > 0 {
                 if inp.key_down(Key::ArrowDown) || inp.key_down(Key::S) {
@@ -165,47 +203,72 @@ impl MyRustSnakeGame
         });
     }
 
-    fn draw_player(
-        &mut self,
-        ui: &egui::Ui,
-    ) {
+    fn draw_player(&mut self, ui: &egui::Ui) {
         for part in self.game_state.body_parts.clone() {
             self.draw_at_cell(ui, self.body_img.clone(), part, self.map_size);
         }
 
-        let head_img = match self.game_state.player_direction {
+        let head_img = match self.game_state.player_draw_direction {
             Direction::TopDown => &self.head_img,
             Direction::BottomUp => &self.head_img_up,
             Direction::LeftToRight => &self.head_img_right,
             Direction::RightToLeft => &self.head_img_left,
         };
-        self.draw_at_cell(ui, head_img.clone(), self.game_state.player_position, self.map_size);
+        self.draw_at_cell(
+            ui,
+            head_img.clone(),
+            self.game_state.player_position,
+            self.map_size,
+        );
+
+        if self.food_location.is_some() {
+            self.draw_at_cell(
+                ui,
+                self.food_img.clone(),
+                self.food_location.unwrap(),
+                self.map_size,
+            );
+        }
     }
 
-    fn game_update(&mut self){
+    fn game_update(&mut self) {
+        let prev_player_pos = self.game_state.player_position;
 
-        self.game_state.body_parts.push_front(self.game_state.player_position);
-        if self.game_state.body_parts.len() > self.game_state.snake_length as usize {
+        // move the head
+        match self.game_state.player_direction {
+            Direction::TopDown => {
+                self.game_state.player_position.0 =
+                    (self.game_state.player_position.0 + 1) % self.map_size.0 as u16;
+            }
+            Direction::BottomUp => {
+                self.game_state.player_position.0 =
+                    (self.game_state.player_position.0 + self.map_size.0 as u16 - 1)
+                        % self.map_size.0 as u16;
+            }
+            Direction::LeftToRight => {
+                self.game_state.player_position.1 =
+                    (self.game_state.player_position.1 + 1) % self.map_size.1 as u16;
+            }
+            Direction::RightToLeft => {
+                self.game_state.player_position.1 =
+                    (self.game_state.player_position.1 + self.map_size.1 as u16 - 1)
+                        % self.map_size.1 as u16;
+            }
+        }
+
+        self.game_state.body_parts.push_front(prev_player_pos);
+
+        if self.game_state.player_position
+            == self
+                .food_location
+                .unwrap_or((self.map_size.0 as u16, self.map_size.1 as u16))
+        {
+            self.food_location = None;
+        } else {
             self.game_state.body_parts.pop_back();
         }
 
-        match self.game_state.player_direction {
-            Direction::TopDown => {
-                self.game_state.player_position.0 = (self.game_state.player_position.0 + 1) % self.map_size.0 as u16;
-            },
-            Direction::BottomUp => {
-                self.game_state.player_position.0 = (self.game_state.player_position.0 + self.map_size.0 as u16 - 1) % self.map_size.0 as u16;
-            },
-            Direction::LeftToRight => {
-                self.game_state.player_position.1 = (self.game_state.player_position.1 + 1) % self.map_size.1 as u16;
-            },
-            Direction::RightToLeft => {
-                self.game_state.player_position.1 = (self.game_state.player_position.1 + self.map_size.1 as u16 - 1) % self.map_size.1 as u16;
-            },
-        }
-        // Here you would also check for collisions with the walls, food, etc.
-
-
+        self.game_state.player_draw_direction = self.game_state.player_direction;
     }
 
     fn draw_at_cell(
@@ -241,5 +304,4 @@ impl MyRustSnakeGame
 
         egui::Image::new(image).paint_at(ui, tile_rect);
     }
-
 }
